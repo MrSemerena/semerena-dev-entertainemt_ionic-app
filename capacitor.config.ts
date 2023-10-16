767f078d7621bf8286c512547caab2f6e40e3970
MrSemerena <mrsemerena@gmail.com>
2023-10-16 10:20:27 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Semerena-DEV-Entertainment',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
